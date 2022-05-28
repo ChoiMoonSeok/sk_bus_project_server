c8ca0e6395b5231f98880dc69e8566e0abc55bb7
ChoiMoonSeok <80397444+ChoiMoonSeok@users.noreply.github.com>
2022-05-29 03:13:33 +0900

    주석 추가

--- acc_data.py
+++ acc_data.py
@@ -1,44 +1,47 @@
+from datetime import datetime
+from xmlrpc.client import DateTime
 from flask import request
 from flask_restx import Resource, Api, Namespace
 
 count = 1
-acc_x = {}
-acc_y = {}
+acc_x = {} # x축 가속도를 저장할 {}
+acc_y = {} # y축 가속도를 저장할 {}
 
 Acc = Namespace('acc_data')
 
 @Acc.route('')
 class AccPost(Resource):
-    def post(self):
+    def post(self): # post 함수 정의, 데이터 입력
         global count
         global acc_datas_raw
 
         idx = count
         count += 1
-        acc_x[idx] = request.json.get('acc_x')
-        acc_y[idx] = request.json.get('acc_y')
+        acc_x[idx] = float(request.json.get('acc_x')) # json을 받아서 float으로 변환 후 {}에 저장
+        acc_y[idx] = float(request.json.get('acc_y')) # json을 받아서 float으로 변환 후 {}에 저장
 
         return {'acc_id' : idx, 'acc_x' : acc_x[idx], 'acc_y': acc_y[idx]}
 
-@Acc.route('/<int:acc_id>')
+@Acc.route('/<int:acc_id>') # acc_id에 맞는 데이터 조회
 class AccSimple(Resource):
-    def get(self, acc_id):
+    def get(self, acc_id): # 데이터 가져오기
         return {
             'acc_id' : acc_id,
             'acc_x': acc_x[acc_id],
-            'acc_y': acc_y[acc_id]
+            'acc_y': acc_y[acc_id],
         }
 
-    def put(self, acc_id):
+
+    def put(self, acc_id): # 데이터 수정하기
         acc_x[acc_id] = request.json.get('acc_x')
         acc_y[acc_id] = request.json.get('acc_y')
         return {
             'acc_id': acc_id,
             'acc_x': acc_x[acc_id],
-            'acc_y': acc_y[acc_id]
+            'acc_y': acc_y[acc_id],
         }
-
-    def delete(self, acc_id):
+ 
+    def delete(self, acc_id): # 데이터 삭제하기
         del acc_x[acc_id]
         del acc_y[acc_id]
         return {
--- server/__init__.py
+++ server/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask, request
 from flask_restx import Api, Resource
 from acc_data import Acc
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask(__name__) 
+api = Api(app) # api 모듈 호출
 
-api.add_namespace(Acc, '/acc_data')
+api.add_namespace(Acc, '/acc_data') # acc_data 파일에 정의한 api 가져오기
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80) # 설정한 주소로 서버 열기
